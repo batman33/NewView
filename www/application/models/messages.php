f44eb172259dab4b1ac9c77bfffded129f864dbe
Serg <batman33@bk.ru>
2013-10-22 10:40:06 +0300

    View Message old don't release

---
+++ www/application/models/messages.php
@@ -0,0 +1,95 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Messages extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	// Таблица данной модели
+	private $table = 'messages';
+
+	public function newMessage($data)
+	{
+		return $this->db->insert($this->table, $data);
+	}
+
+	public function getAllMessage($id)
+	{
+		$this->db->select('
+			messages.id as mess_id,
+			messages.title as mess_title,
+			messages.status as mess_status,
+			messages.created as mess_created,
+			users.name as u_name,
+			users.id as u_id');
+
+		$this->db->order_by('messages.id', 'desc');
+		
+		$this->db->where('to', $id);
+		$this->db->join('users', 'users.id = messages.from', 'left');
+
+        $query = $this->db->get($this->table);
+        return $query->result();		
+	}
+
+	public function getAllSentMessage($id)
+	{
+		$this->db->select('
+			messages.id as mess_id,
+			messages.title as mess_title,
+			messages.status as mess_status,
+			messages.created as mess_created,
+			users.name as u_name,
+			users.id as u_id');
+		
+		$this->db->where('from', $id);
+		$this->db->join('users', 'users.id = messages.to', 'left');
+
+        $query = $this->db->get($this->table);
+        return $query->result();		
+	}
+
+	public function getMessageById($id)
+	{
+        if($id){
+
+			$this->db->select('
+				messages.id as mess_id,
+				messages.title as mess_title,
+				messages.message as messages,
+				messages.status as mess_status,
+				messages.created as mess_created,
+				users.name as u_name,
+				users.id as u_id');
+
+            $this->db->where('messages.id', $id);
+			$this->db->join('users', 'users.id = messages.from', 'left');            
+            $this->db->from($this->table);       
+            $query = $this->db->get();
+            $result = $query->result();
+            return $result[0];
+        } else {
+            return false;
+        }        	
+	}
+
+	public function deActive($id, $data)
+	{
+		$this->db->where('id', $id);
+		$this->db->update($this->table, $data);
+	}
+
+	public function countByUser($id)
+	{
+		$this->db->where('to', $id);
+		$this->db->where('status', '1');
+		$this->db->from($this->table); 
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+}
+
+/* End of file messages.php */
+/* Location: ./application/models/messages.php */
---
+++ www/application/views/user/u_view_message.php
@@ -0,0 +1,16 @@
+<div class="content full">
+	<h2>Сообщение <?php echo anchor('/user/message', 'Вернутся', 'class="header-link"'); ?></h2><br />
+
+	<div class="view-single-message">
+		<div class="title">
+			<h3 class="title message"><?=$message->mess_title?> <?=anchor('/user/deleteMessage/' . $message->mess_id, 'Удалить сообщение');?></h3>
+		</div>
+		<div>
+			<span>Отправлено: <?=mdate("%d.%m.%Y г. %h:%i",$message->mess_created)?></span>
+			<span>От: <?=anchor('/user/viewUser/' . $message->u_id, $message->u_name);?></span>
+		</div>
+
+		<p><?=$message->messages?></p>
+	</div>
+
+</div>
