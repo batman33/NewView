756564a0262ffad6dbd975c3fd61e097c675d29f
Serg <5735454+batman33@users.noreply.github.com>
2024-06-10 01:57:05 +0300

    Delete www/application/logs/log-2013-10-16.php

--- www/application/logs/log-2013-10-16.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-10-16 20:33:32 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-10-16 22:25:41 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-10-16 22:36:43 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-10-16 23:02:07 --> Severity: Notice  --> Undefined variable: data S:\home\newview\www\application\controllers\home.php 320
-ERROR - 2013-10-16 23:02:45 --> Severity: Notice  --> Undefined variable: data S:\home\newview\www\application\controllers\home.php 320
