5a80ebd99189c332098020529c743dd373a5672e
Serg <5735454+batman33@users.noreply.github.com>
2024-06-10 01:58:52 +0300

    Update config.php

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://newview.loc';
+$config['base_url']	= 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
@@ -69,7 +69,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'russian';
+$config['language']	= 'en';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'newview';
+$config['encryption_key'] = 'Px0vUObFC9c3%!eH';
 
 /*
 |--------------------------------------------------------------------------
