63098b282d2ece9d6b2cc79256dcd0e520b72de2
Serg <5735454+batman33@users.noreply.github.com>
2024-06-10 01:56:55 +0300

    Delete www/application/logs/log-2013-10-15.php

--- www/application/logs/log-2013-10-15.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-10-15 21:26:29 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-10-15 22:28:50 --> Severity: Notice  --> Undefined property: User::$form S:\home\newview\www\application\controllers\user.php 160
