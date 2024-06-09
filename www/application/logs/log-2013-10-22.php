8d43286abafaed5d071f2471347934142b07b232
Serg <5735454+batman33@users.noreply.github.com>
2024-06-10 01:57:26 +0300

    Delete www/application/logs/log-2013-10-22.php

--- www/application/logs/log-2013-10-22.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-10-22 12:31:03 --> 404 Page Not Found --> favicon.ico
-ERROR - 2013-10-22 12:32:18 --> 404 Page Not Found --> user/viewUser
-ERROR - 2013-10-22 18:05:11 --> Query error: Not unique table/alias: 'users'
-ERROR - 2013-10-22 18:10:39 --> Severity: Notice  --> Undefined variable: status C:\server\myproject\NewView\www\application\models\messages.php 70
-ERROR - 2013-10-22 18:17:25 --> 404 Page Not Found --> user/viewUser
-ERROR - 2013-10-22 18:21:45 --> Severity: Notice  --> Undefined variable: countMessage C:\server\myproject\NewView\www\application\views\user\block\top.php 58
